d06e19900413c25ca42b091d76c6e9a68c5c5489
Annie Ellenberger <annie.ellenberger@gmail.com>
2025-10-31 12:30:02 -0400

    Fix favicon: add trailing slash to base and set trailingSlash config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://annieellenberger.github.io",
-  base: process.env.NODE_ENV === 'production' ? "/ae-cto" : "/",
+  base: process.env.NODE_ENV === 'production' ? "/ae-cto/" : "/",
+  trailingSlash: "always",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
